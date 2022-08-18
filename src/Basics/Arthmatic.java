4ee21b22691c1d3b4ea4891d7d05c1e9cc2907ec
Feroz Bux <ferozbux24000@gmail.com>
2022-08-18 21:35:23 +0530

    initiat setup

---
+++ src/Basics/Arthmatic.java
@@ -0,0 +1,21 @@
+package Basics;
+import java.util.Scanner;
+public class Arthmatic {
+public static void main (String args[])
+{
+	String n;
+	int n1, n2, n3;
+	try (Scanner sc = new Scanner(System.in)) {
+		System.out.println("Enter your name");
+		n = sc.next();
+		
+		System.out.println("Enter the 3 numbers and press enter after entering each number");
+ 
+			n1 = sc.nextInt();
+			n2 = sc.nextInt();
+			n3 = sc.nextInt();
+	}
+	System.out.println(n +" TheSum = " + (n1+n2+n3));
+
+}
+}
