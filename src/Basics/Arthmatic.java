a27a6896328031ea09aa60708bb28d6751b00ed1
Feroz Bux <70798068+ferozbux01@users.noreply.github.com>
2022-08-18 21:59:46 +0530

    test

--- src/Basics/Arthmatic.java
+++ src/Basics/Arthmatic.java
@@ -1,7 +1,7 @@
 package Basics;
 import java.util.Scanner;
 public class Arthmatic {
-public static void main (String args[])
+public static void main (String args[]) Delete this line
 {
 	String n;
 	int n1, n2, n3;
